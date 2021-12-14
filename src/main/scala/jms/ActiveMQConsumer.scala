f6886d6f2742e549cb9acb1550875408c631fb38
RSaravanaPrasad <88064225+RSaravanaPrasad@users.noreply.github.com>
2021-12-14 20:19:33 +0530

    Naming modified
    Naming modified

--- src/main/scala/jms/ActiveMQConsumer.scala
+++ src/main/scala/jms/ActiveMQConsumer.scala
@@ -1,19 +1,20 @@
 import javax.jms._
 import org.apache.activemq.ActiveMQConnectionFactory
- 
+
 object ActiveMQConsumer {
   val activeMqUrl: String = "tcp://localhost:61616"
+
   def main(args: Array[String]) {
-    val connFactory = new ActiveMQConnectionFactory()
-    val conn = connFactory.createConnection()
-    val sess = conn.createSession(false, Session.AUTO_ACKNOWLEDGE)
-    val dest = sess.createQueue("messageQueue")
-    val cons = sess.createConsumer(dest)
-    conn.start()
+    val connectionFactory = new ActiveMQConnectionFactory()
+    val connection = connectionFactory.createConnection()
+    val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+    val destination = session.createQueue("messageQueue")
+    val consumer = session.createConsumer(destination)
+    connection.start()
     while (true) {
-      val msg = cons.receive()
-      println(msg)
+      val message = consumer.receive()
+      println(message)
     }
-    conn.close()
+    connection.close()
   }
 }
