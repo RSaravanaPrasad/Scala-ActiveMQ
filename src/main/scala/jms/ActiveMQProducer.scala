fe7c615ceee8160651790a2d3e8ed29c3bdf3717
RSaravanaPrasad <88064225+RSaravanaPrasad@users.noreply.github.com>
2021-12-14 20:18:40 +0530

    Naming modified
    Naming modified

--- src/main/scala/jms/ActiveMQProducer.scala
+++ src/main/scala/jms/ActiveMQProducer.scala
@@ -1,19 +1,17 @@
 import javax.jms._
 import org.apache.activemq.ActiveMQConnectionFactory
-import util.Random
-import java.time.LocalDateTime;
- 
+
 object ActiveMQProducer {
   val activeMqUrl: String = "tcp://localhost:61616"
 
   def main(args: Array[String]) {
-    val connFactory = new ActiveMQConnectionFactory()
-    val conn = connFactory.createConnection()
-    val sess = conn.createSession(false, Session.AUTO_ACKNOWLEDGE)
-    val dest = sess.createQueue("messageQueue")
-    val prod = sess.createProducer(dest)
-    val msg = sess.createTextMessage("Simples Assim")
-    prod.send(msg)
-    conn.close()
+    val connectionFactory = new ActiveMQConnectionFactory()
+    val connection = connectionFactory.createConnection()
+    val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+    val destination = session.createQueue("messageQueue")
+    val producer = session.createProducer(destination)
+    val message = session.createTextMessage("Simples Assim")
+    producer.send(message)
+    connection.close()
   }
 }
